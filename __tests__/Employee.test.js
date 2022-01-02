e4541489c76201c847951b2630cf80c138a6bb2c
camyacodes <camyacodes@gmail.com>
2022-01-01 23:46:37 -0500

    refactored code and added all methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,23 +1,35 @@
 const { expect } = require('@jest/globals');
-const Employee = require('../lib/Employee.js');
+const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Mya');
+    const employee = new Employee('Mya', 5, 'code@code.com');
 
-    expect(employee.name).toBe('Mya');
-    expect(employee.id).toBe(5);
-    expect(employee.email).toBe('code');
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 
 })
 
 test("gets employee's name", () => {
-    const employee = new Employee('Mya');
+    const employee = new Employee('Mya', 5, 'code@code.com');
 
-    expect(employee.getName()).toHaveProperty('name', 'Mya')
+    expect(employee.getName()).toHaveProperty('name')
 })
 
-// test("gets employee's id", () => {
-//     const employee = new Employee('Mya');
+test("gets employee's id", () => {
+    const employee = new Employee('Mya', 5, 'code@code.com');
 
-//     expect(employee.getId()).toHaveProperty('id', 5)
-// })
+    expect(employee.getId()).toHaveProperty('id')
+})
+
+test("gets employee's email", () => {
+    const employee = new Employee('Mya', 5, 'code@code.com');
+
+    expect(employee.getEmail()).toHaveProperty('email')
+})
+
+test("gets employee's role", () => {
+    const employee = new Employee('Mya', 5, 'code@code.com');
+
+    expect(employee.getRole()).toEqual('Employee')
+})
